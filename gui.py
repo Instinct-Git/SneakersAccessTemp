322ab0a99675f5d46c2425850208b714a584106a
Instinct-Git <69330184+Instinct-Git@users.noreply.github.com>
2020-08-09 01:58:52 +0100

    Code Cleanup
    - Code has been cleaned up

--- gui.py
+++ gui.py
@@ -4,20 +4,24 @@ from flaskwebgui import FlaskUI
 app = Flask(__name__)
 ui = FlaskUI(app)
 
-@app.route("/", methods=['POST','GET'])
+
+@app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
     else:
         return render_template('index.html')
 
+
 @app.route("/profiles")
 def profiles():
     return render_template('profiles.html')
 
+
 @app.route("/captcha")
 def captcha():
     return render_template('captcha.html')
+
 
 if __name__ == "__main__":
     ui.run()
